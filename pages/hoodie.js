c7336cd75e6f35d34135ba8a0afea5df7eb6c7e2
Hardik Desai <124128351+hardikd-zignuts@users.noreply.github.com>
2023-03-28 15:02:48 +0530

    refactor: :recycle: Setup With Pages

---
+++ pages/hoodie.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hoodie = () => {
+  return (
+    <div>
+      Hoodie
+    </div>
+  )
+}
+
+export default Hoodie
---
+++ pages/mug.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Mug = () => {
+  return (
+    <div>
+      mug
+    </div>
+  )
+}
+
+export default Mug
